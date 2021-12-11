9a06c02e3ee0c6703c9f1f02867e70aa34a8c84a
GMBuzatto <gabrielmartinbuzatto1999@gmail.com>
2021-12-10 22:12:27 -0300

    HelloWorld

---
+++ Exercicio de fixacao/Service/IOnlinePaymentService.cs
@@ -0,0 +1,8 @@
+﻿namespace Exercicio_de_fixacao.Service
+{
+    internal interface IOnlinePaymentService
+    {
+        double PaymentFee(double amount);
+        double Interest(double amount, int months);
+    }
+}
